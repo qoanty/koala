495cd90ed4510cde19aca632c29bb0ce48d47cc2
qoanty <qoanty@gmail.com>
2019-04-01 12:06:13 +0800

    add publish date

---
+++ allbids.py
@@ -0,0 +1,137 @@
+#coding:utf-8
+__author__ = 'chenke'
+
+"""
+查询采购与招标网中风力发电机组的招标信息
+获取验证码的地址并下载，输入验证码后登录
+以关键字“风电”进行搜索，获取标题及地址
+筛选标题去除相关条目后进一步搜索公告内容
+显示最终结果的地址及标题并打开相应的网页
+data: 2018/8/30
+version: Python3.6
+"""
+
+from requests_html import HTMLSession
+import os, sys, time, webbrowser
+from tomorrow import threads
+
+class Bidding:
+    def __init__(self, url, page):
+        self.url = url
+        self.loginurl = self.url + '/cblcn/member.login/login'
+        self.page = page
+        self.key_title1 = '风电'
+        self.key_title2 = '风力'
+        self.key_content = '风力发电机组'
+        self.exp_list = ['询价','施工','维修','维护','运维','改造','改建','中标','塔筒','塔架','基础','法兰','锚栓','压站','主轴','变压器','变流器','主变','箱变','勘察','设计','线路','道路','监理','备件','吊装','可行性','可研','装置','检测','检修','监测','监督','测试','测评','试验','变更','更换','技改','验收','安装','分包','电缆','光缆','材料','箱式','框架','造价','通信','编码','定检','叶片','倒运','消防','开关','主体','集控','诊断','齿轮','柴油','部件','电池','风扇','充电','故障','消缺','外委','水土','电容','稳控','变桨','滑环','打捆','咨询','测风','风速仪','电压','电源','电阻','电梯','模块','网关','数据','驱动','配件','刹车','升降','防尘','评估','档案','监控','偏航','标识','土建','振动','仿真','通讯','液压','雷电','租赁','端子','紫铜','蓄能','加热','控制','继电器','接口','导流','变频','工控','滤芯','螺栓','熔断器','交换机','集电环','联轴器','电气'] # 电机
+        self.sel_title = 'tbody tr td a'
+        self.sel_content = 'div.xq_nr'
+        self.sel_pubdate = 'div.xiab_1 > span'
+        self.session = HTMLSession() # 获取session对象，可自动记录Cookies值
+        self.headers = {
+                'User-Agent':'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.108 Safari/537.36',
+                'referer' : 'https://www.chinabidding.cn/',
+                }
+        self.data = {}
+        self.t = time.time()
+        self.stamp = str(round(self.t * 1000)) #获取毫秒级时间戳
+        self.now = time.strftime('%Y-%m-%d', time.localtime(self.t)) #获取当前日
+        self.logindata = {
+                'name' : '联合动力',
+                'password' : 'lhdlgdupc5765',
+                }
+
+    def get_result(self):
+        start = time.time()
+        if self.chkyzm(): #获取验证码、下载、输入并验证
+            response = self.session.post(self.loginurl, data=self.logindata, headers=self.headers)
+            #print(response.text) #登录成功自动跳转到首页
+            for i in range(1, self.page):
+                searchurl = self.url + '/search/searchgj/zbcg'
+                self.data = {
+                        'areaid' : '',
+                        'keywords' : '风电',
+                        'time_start' : self.now,
+                        'time_end' : self.now,
+                        'page' : i,
+                        'search_type' : 'CONTEXT',
+                        'categoryid' : '',
+                        'rp' : '30',
+                        'table_type' : '',
+                        'b_date' : 'week',
+                        }
+                response = self.get_response(searchurl)
+                item_list = response.html.find(self.sel_title)
+                #print(item_list)
+                for item in item_list:
+                    try:
+                        href = self.url + item.attrs['href']
+                        title = item.attrs['title']
+                    except:
+                        continue
+                    #title = item.text
+                    #print(title)
+                    if self.chktitle(title): #筛选标题去除相关条目
+                        if self.key_title1 in title or self.key_title2 in title:
+                            response = self.get_response(href)
+                            try:
+                                content = response.html.find(self.sel_content, first=True).text
+                                pubdate = response.html.find(self.sel_pubdate, first=True).text
+                            except Exception as e:
+                                print(str(e))
+                                continue
+                            #print(content)
+                            if self.key_content in content:
+                                webbrowser.open(href)
+                                print(href, pubdate, title)
+            time.sleep(2)
+        end = time.time() - start
+        print('耗时：%.2f 秒' %end)
+        print(time.strftime("%H:%M:%S")) # 当前时间
+
+    def chkyzm(self):
+        response = self.get_response(self.loginurl) # 获取登录页面的验证码信息
+        img = response.html.find('#img_random', first=True).attrs['src']
+        userid = img.split('=')[1] # 获取randomID
+        yzmsrc = self.url + img + '&t=' + self.stamp # 获取验证码地址
+        #print(yzmsrc, userid)
+        yzmpic = self.get_response(yzmsrc) # 获取验证码图片并保存
+        f = open('captcha.png', 'wb')
+        f.write(yzmpic.content)
+        f.close()
+
+        os.system('start captcha.png') # 显示验证码图片
+        yzm = input('请输入验证码：')
+        #chkyzmurl = self.url + '/cblcn/member.login/checkyzm?randomID=' + userid + '&yzm=' + yzm + '&t=' + self.stamp # 检测验证码地址
+        chkyzmurl = self.url + '/cblcn/member.login/checkyzm'
+        self.data = {'randomID':userid, 'yzm':yzm, 't':self.stamp }
+        #print(chkyzmurl)
+        chk = self.get_response(chkyzmurl)
+        if chk.text == 'true':
+            print('验证码通过')
+            return True
+        else:
+            print('验证码失败')
+            return False
+
+    def chktitle(self, title):
+        for word in self.exp_list:
+            if word in title:
+                #print(word)
+                return False
+        #print(title)
+        return True
+
+    @threads(5)
+    def get_response(self, url):
+        r = self.session.get(url, params=self.data, headers=self.headers)
+        return r
+
+if __name__ == "__main__":
+    try:
+        page = int(sys.argv[1])
+    except:
+        print('用法: ' + sys.argv[0] + ' num 查询页数')
+        sys.exit()
+    chinabidding = Bidding('https://www.chinabidding.cn', page)
+    chinabidding.get_result()
---
+++ groupbids.py
@@ -0,0 +1,356 @@
+#coding:utf-8
+__author__ = 'chenke'
+
+from requests_html import HTMLSession
+import requests, time, datetime
+import webbrowser
+import warnings
+from tomorrow import threads
+from colorama import init, Fore, Back, Style
+
+class Bids:
+    def __init__(self):
+        self.key_title = '风力发电'         # 标题关键字
+        self.key_title_else = '风电机组'    # 标题关键字
+        self.key_content = '风力发电机组'   # 内容关键字
+        self.sel_title = ''
+        self.sel_content = ''
+        self.sel_pubdate = ''
+        self.session = HTMLSession()
+        init(autoreset=True)  # 初始化并设置颜色自动恢复
+
+    def get_result(self):
+        response = self.get_response(self.url)
+        if response:    # 获取对象不为空
+            item_list = response.html.find(self.sel_title)
+            #print(item_list)   # 标题列表
+            for item in item_list:
+                href = self.get_href(item)      # 获取URL
+                title = self.get_title(item)    # 获取标题文本
+                #title = item.text
+                if self.key_title in title or self.key_title_else in title:
+                    response = self.get_response(href)
+                    content = response.html.find(self.sel_content, first=True).text
+                    pubdate = response.html.find(self.sel_pubdate, first=True).text
+                    if '：' in pubdate:
+                        pdate = pubdate.split(' ')[0].split('：')[1]
+                    else:
+                        pdate = pubdate.split(' ')[0]
+                    #print(href, pdata, title)         # 查询内容的标题
+                    #print(content)
+                    if self.key_content in content:
+                        #webbrowser.open(href)
+                        print(href, pdate, title)
+        else:
+            print(Fore.RED + '查询的网页已失效，请检查网址！！！')
+        #print('查询页面：', self.url)
+
+    def get_href(self, item):
+        return item.attrs['href']
+
+    def get_title(self, item):
+        try:
+            return item.attrs['title']
+        except:
+            return ''
+
+    @threads(3) #多线程
+    def get_response(self, url):
+        try:
+            headers = {
+                    'User-Agent':'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.108 Safari/537.36'
+                    }
+            #proxies = {'http': 'http://219.141.153.41:80'}
+            proxies = {'http': ''}
+            r = self.session.get(url, headers=headers, proxies=proxies, timeout=8) # verify = False
+            r.encoding = 'utf-8'
+            return r
+        except requests.exceptions.RequestException as e:
+            print(Fore.RED + '连接错误！')
+            return None
+
+class Gdzb(Bids): #国电招标
+    def __init__(self):
+        super().__init__()
+        self.sel_title = 'div.lb-link > ul > li > a'
+        self.sel_content = 'div.WordSection1'
+        self.sel_pubdate = 'div.ninfo-title > span'
+
+    def get_result(self):
+        for i in range(1):
+            self.url = 'http://www.cgdcbidding.com/ggsb/index.jhtml'
+            super().get_result()
+        print(Fore.BLUE + '国电招标公告查询完毕！')
+
+class Hrzb(Bids): #华润招标（网页编码）
+    def __init__(self):
+        super().__init__()
+        self.key_title_else = '风电机组'
+        self.sel_title = 'tbody > tr > td > a'
+        self.sel_content = 'table'
+        self.sel_pubdate = 'div.ewb-con-info > span'
+
+    def get_result(self):
+        for i in range(1, 4):
+            if i == 1:
+                self.url = 'http://www.crpsz.com/zbxx/006001/006001001/secondpagejy.html'
+            else:
+                self.url = 'http://www.crpsz.com/zbxx/006001/006001001/%d.html' % i
+            super().get_result()
+        print(Fore.YELLOW + '华润招标公告查询完毕！')
+
+    def get_href(self, item):
+        return 'http://www.crpsz.com' + item.attrs['href']
+
+    def get_title(self, item):
+        try:
+            return item.text
+        except:
+            return ''
+
+class Zghzb(Bids): #中广核招标（动态加载）
+    def __init__(self):
+        super().__init__()
+        self.sel_title = 'ul > li > a'
+        self.sel_content = 'div.frameReport'
+        self.sel_pubdate = 'span.publishTime'
+
+    def get_result(self):
+        warnings.filterwarnings("ignore") #忽略InsecureRequestWarning
+        for i in range(1, 3):
+            self.url = 'https://ecp.cgnpc.com.cn/CmsNewsController.do?method=recommendBulletinList&index=provincebuyBulletin&channelCode=zgh_zbgg&rp=20&page=%d' % i
+            super().get_result()
+        print(Fore.MAGENTA + '中广核招标公告查询完毕！')
+
+    def get_href(self, item):
+        return 'https://ecp.cgnpc.com.cn' + item.attrs['href']
+
+class Hdzb(Bids): #华电招标（证书验证）
+    def __init__(self):
+        super().__init__()
+        self.key_title_else = '风电机组'
+        self.sel_title = 'a'
+        self.sel_content = 'div.Basic_information'
+        self.sel_pubdate = 'div.headline > dl > dd'
+
+    def get_result(self):
+        warnings.filterwarnings("ignore") #忽略InsecureRequestWarning
+        for i in range(1, 2):
+            self.url = 'https://www.chdtp.com/webs/queryWebZbgg.action?zbggType=1&page.pageSize=31&page.currentpage=%d' % i
+            super().get_result()
+        print(Fore.RED + '华电招标公告查询完毕！')
+
+    def get_href(self, item):
+        return 'https://www.chdtp.com/staticPage/' + item.attrs['href'].split("'")[1]
+
+class Hnzb(Bids): #华能招标
+    def __init__(self):
+        super().__init__()
+        self.sel_title = 'ul.main_r_con > li > a'
+        self.sel_content = 'div.detail_box'
+        self.sel_pubdate = 'i'
+
+    def get_result(self):
+        for i in range(5):
+            self.url = 'http://ec.chng.com.cn/ecmall/more.do?type=103&start=%d&limit=10' % (i*10)
+            super().get_result()
+        print(Fore.CYAN + '华能招标公告查询完毕！')
+
+    def get_href(self, item):
+        return 'http://ec.chng.com.cn/ecmall/announcement/announcementDetail.do?announcementId=' + item.attrs['href'].split("'")[1]
+
+class Hbzb(Bids): #河北建投招标
+    def __init__(self):
+        super().__init__()
+        self.sel_title = 'ul.ewb-con-item > li > a'
+        self.sel_content = 'div.details-info'
+        self.sel_pubdate = 'span.article-date'
+
+    def get_result(self):
+        for i in range(1, 4):
+            self.url = 'http://hebeibidding.com/TPFront/xmxx/016001/?Paging=%d' % i
+            super().get_result()
+        print(Fore.GREEN + '河北建投招标公告查询完毕！')
+
+    def get_href(self, item):
+        return 'http://hebeibidding.com' + item.attrs['href']
+
+class Shzb(Bids): #神华招标
+    def __init__(self):
+        super().__init__()
+        self.key_title = '风电'
+        self.sel_title = 'ul.right-items > li > div > a'
+        self.sel_content = 'div.article'
+        self.sel_pubdate = 'p.info-sources'
+
+    def get_result(self):
+        for i in range(1, 5):
+            self.url = 'http://www.shenhuabidding.com.cn/bidweb/001/001002/%d.html' % i
+            super().get_result()
+        print(Fore.MAGENTA + '神华招标公告查询完毕！')
+
+    def get_href(self, item):
+        return 'http://www.shenhuabidding.com.cn' + item.attrs['href']
+
+class Xhzb(Bids): #协合招标
+    def __init__(self):
+        super().__init__()
+        self.key_title = '风电'
+        self.sel_title = 'div.news-wrap > ul > li > a'
+        self.sel_content = 'div.detail-content'
+        self.sel_pubdate = 'p.datetime'
+
+    def get_result(self):
+        self.url = 'http://www.cnegroup.com/zh/bid/index.html'
+        super().get_result()
+        print(Fore.BLUE + '协合招标公告查询完毕！')
+
+    def get_href(self, item):
+        return 'http://www.cnegroup.com' + item.attrs['href']
+
+    def get_title(self, item):
+        try:
+            return item.text
+        except:
+            return ''
+
+class Gjdtzb(Bids): #国家电投招标
+    def __init__(self):
+        super().__init__()
+        self.sel_title = 'div.article_list_lb ul li a'
+
+    def get_result(self):
+        self.url = 'http://www.cpeinet.com.cn/cpcec/bul/bul_list.jsp?type=1'
+        response = self.get_response(self.url)
+        item_list = response.html.find(self.sel_title)
+        for item in item_list:
+            page = item.attrs['onclick'].split('(')[1].split(',')[0]
+            href = 'http://www.cpeinet.com.cn/cpcec/bul/bulletin_show.jsp?id=' + page
+            title = item.attrs['title']
+            if self.key_title in title:
+                print(href, title)
+        print(Fore.CYAN + '国家电投招标公告查询完毕！')
+
+class Sxzb(Bids): #三峡招标（动态加载，获取json文件）
+    def __init__(self):
+        super().__init__()
+        self.key_title = '风电'
+        self.sel_title = ''
+        self.sel_content = 'html'
+
+    def get_result(self):
+        for i in range(2):
+            self.url = 'http://epp.ctg.com.cn/index/getData.do?queryName=ctg.list.zbgg&page=%d&rows=15' % i
+            response = self.get_response(self.url)
+            item_list = response.json()['rows']
+            for item in item_list:
+                t = time.time()
+                href = 'http://epp.ctg.com.cn/static/temphtml/' + item['ARTICLE_ID'] + '.html?_=' + str(round(t * 1000)) #毫秒级时间戳
+                title = item['TITLE']
+                if self.key_title in title:
+                    response = self.get_response(href)
+                    content = response.html.find(self.sel_content, first=True).text
+                    if self.key_content in content:
+                        #webbrowser.open(href)
+                        print(href, title)
+        print(Fore.YELLOW + '三峡招标公告查询完毕！')
+
+class Dtzb(Bids): #大唐招标
+    def __init__(self):
+        super().__init__()
+
+    def get_result(self):
+        for i in range(1, 5):
+            self.url = 'http://www.cdt-ec.com/potal-web/pendingGxnotice/where?message_type=0&pageno=%d&pagesize=15' % i
+            response = self.get_response(self.url)
+            item_list = response.json()
+            for item in item_list:
+                title = item['message_title']
+                href = item['pdf_url']
+                if self.key_title in title:
+                    print(href, title)
+        print(Fore.RED + '大唐招标公告查询完毕！')
+
+class Cebpub(Bids): #招投标公共服务平台
+    def __init__(self):
+        super().__init__()
+        self.sel_title = 'table tr td a'
+
+    def get_result(self):
+        today = datetime.date.today()
+        date = today + datetime.timedelta(days=-2)
+        print(today)
+        try:
+            headers = {
+                    'User-Agent':'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.108 Safari/537.36'
+                    }
+            searchurl = 'http://bulletin.cebpubservice.com/xxfbcmses/search/bulletin.html'
+            for i in range(1, 6):
+                data = {
+                    'searchDate' : date,
+                    'dates' : 3,
+                    'word' : self.key_content,
+                    'categoryId' : 88,
+                    'industryName' : '',
+                    'area' : '',
+                    'status' : '',
+                    'publishMedia' : '',
+                    'sourceInfo' : '',
+                    'showStatus' : '',
+                    'page' : i,
+                    }
+                response = self.session.get(searchurl, headers=headers, params=data, timeout=8)
+                item_list = response.html.find(self.sel_title)
+                #print(item_list)
+                if item_list:
+                    for item in item_list:
+                        href = self.get_href(item).split("'")[1]
+                        pdate = href.split('/')[4]
+                        title = self.get_title(item)
+                        if self.key_content in title:
+                            print(href, '\n', pdate, title)
+        except requests.exceptions.RequestException as e:
+            print(e)
+        print(Fore.GREEN + '招投标公共平台查询完毕！')
+
+if __name__ == "__main__":
+    #招投标公共平台
+    cebpub = Cebpub()
+    cebpub.get_result()
+    #国电招标
+    gdzb = Gdzb()
+    gdzb.get_result()
+    #华润招标
+    hrzb = Hrzb()
+    hrzb.get_result()
+    #华电招标
+    hdzb = Hdzb()
+    hdzb.get_result()
+    #华能招标
+    hnzb = Hnzb()
+    hnzb.get_result()
+    #神华招标
+    shzb = Shzb()
+    shzb.get_result()
+    #河北建投招标
+    hbzb = Hbzb()
+    hbzb.get_result()
+    #协合招标
+    xhzb = Xhzb()
+    xhzb.get_result()
+    #三峡招标
+    sxzb = Sxzb()
+    sxzb.get_result()
+    #大唐招标
+    dtzb = Dtzb()
+    dtzb.get_result()
+    #国家电投招标
+    gjdtzb = Gjdtzb()
+    gjdtzb.get_result()
+    #中广核招标
+    zghzb = Zghzb()
+    zghzb.get_result()
+
+    from allbids import *
+    chinabidding = Bidding('https://www.chinabidding.cn', 20)
+    chinabidding.get_result()
